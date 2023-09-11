d64374f032dfe932ba4cb89f44b193a13bb5a163
kinchan0815 <yo.kanayama0815@gmail.com>
2023-09-11 13:52:27 +0900

    submit

---
+++ todoproject/todo_app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from . models import *
+from .forms import TaskForm
+
+
+# Create your views here.
+def index(request):
+    tasks = task.objects.all()
+    form = TaskForm()
+    if request.method == "POST":
+        form = TaskForm(request.POST) 
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+    context = {"tasks":tasks,"form":form}
+    return render(request,"list.html",context)
+
+def update_task(request,pk):
+    tasks = task.objects.get(id = pk)
+    form = TaskForm(instance=tasks)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST,instance=tasks)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {"form":form}
+    
+    return render(request,"update_task.html",context)
+
+def deletetask(request,pk):
+    item = task.objects.get(id = pk)
+    if request.method == "POST":
+        item.delete()
+        return redirect("/")
+
+    context = {"item":item}
+    return render(request, "delete.html",context)
+
+
